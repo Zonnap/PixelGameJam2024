10cfd3f2bc3419e6a2e3aa286e5e82f1a7a9045a
Zonnap <Zonnap@outlook.com>
2024-05-16 15:58:11 -0400

    Fishing is cut, added FPS stuff

--- PGJ24_Game/Scripts/init_weapon.gd
+++ PGJ24_Game/Scripts/init_weapon.gd
@@ -2,7 +2,11 @@
 
 extends Node3D
 
-@export var WEAPON_TYPE: Weapons
+@export var WEAPON_TYPE: Weapons:
+	set(value):
+		WEAPON_TYPE = value
+		if Engine.is_editor_hint():
+			load_weapon()
 
 @onready var weapon_mesh : MeshInstance3D = %WeaponMesh
 @onready var weapon_shadow : MeshInstance3D = %WeaponShadow
@@ -10,6 +14,14 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	load_weapon()
+	
+func _input(event):
+	if event.is_action_pressed("Weapon1"):
+		WEAPON_TYPE = load("res://Resources/Crowbar/Crowbar.tres")
+		load_weapon()
+	if event.is_action_pressed("Weapon2"):
+		WEAPON_TYPE = load("res://Resources/Crowbar_Left/Crowbar2.tres")
+		load_weapon()
 
 func load_weapon() -> void:
 	if WEAPON_TYPE != null:
